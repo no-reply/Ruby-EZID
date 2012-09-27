832eb14b51bcebe5154c67de98c8f27cc2d084a0
Thomas Johnson <thomas.johnson@oregonstate.edu>
2012-09-26 18:23:02 -0700

    make call_api method work. This is now a (minimally) useful, if untested api.

--- ezid.rb
+++ ezid.rb
@@ -1,5 +1,4 @@
-require 'rest_client'
-require 'uri'
+require 'net/http'
 
 module Ezid
 
@@ -8,7 +7,7 @@ module Ezid
     VERSION = '0.2'
     APIVERSION = 'EZID API, Version 2'
 
-    SECURESERVER = URI.parse("https://n2t.net/ezid")
+    SECURESERVER = "https://n2t.net/ezid"
     TESTUSERNAME = 'apitest'
     TESTPASSWORD = 'apitest'
     SCHEMES = {:ark => 'ark:/', :doi => "doi:"}
@@ -28,70 +27,117 @@ r', 'erc.what' => 'The Open Society and Its Enemies', 'erc.when' => '1945'}
         @test = false
       end
 
-      @username = username
+      @user = username
       @pass = password
       @scheme = SCHEMES[scheme]
       @naa = naa
 
-      
+      if @test == true
+        @naa = TESTSHOULDER[@scheme]
+      end
     end
 
     def mint(metadata={})
       shoulder = @scheme + @naa
       metadata['_status'] = PRIVATE
       #TODO: send mint request to API
-      return false
+      false
     end
 
     def create(identifier, metadata={})
       if not metadata['_status']
         metadata['_status'] = PRIVATE
       end
-      if not identifier.start_with?(SCHEMES[:ark]) or identifier.start_with(SCHEMES[:doi])
+      if not (identifier.start_with?(SCHEMES[:ark]) or identifier.start_with?(SCHEMES[:doi]))
         identifier = @scheme + @naa + identifier
       end
-      request_uri = SECURESERVER + "/id/" + identifier
-      call_api(request_uri, :put, make_anvl(metadata))
+      request_uri = "/id/" + identifier
+      call_api(request_uri, :put, metadata)
     end
 
     def modify(identifier, name, value)
-
+      request_uri = "/id/" + identifier
+      call_api(request_uri, :post, {name => value})
     end
 
     def get(identifier)
-      request_uri = SECURESERVER + "/id/" + identifier
+      request_uri = "/id/" + identifier
       call_api(request_uri, :get)
     end
 
     def delete(identifier)
-
+      request_uri = "/id/" + identifier
+      call_api(request_uri, :delete)
     end
 
+    # public utility methods
     def change_profile(identifier, profile)
-
+      modify(identifier, '_profile', profile)
     end
 
     def get_status(identifier)
       get(identifier)['metadata']['_status']
     end
 
+    def make_public(identifier)
+      modify(identifier, '_status', PUBLIC)
+    end
+
+    def make_unavailable(identifier)
+      modify(identifier, '_status', UNAVAIL)
+    end
+
     def get_target(identifier)
       get(identifier)['metadata']['_target']
     end
 
+    def modify_target(identifier, target)
+      modify(identifier, '_target', target)
+    end
+
+    def record_modify(identifier, metadata, clear=false)
+      if clear
+        #TODO: clear old metadata
+      end
+      metadata.each do |name, value|
+        modify(identifier, name, value)
+      end
+      get(identifier)
+    end
+
+    def set_scheme(scheme)
+      @scheme = scheme
+    end
+
+    def set_naa(naa)
+      @naa = naa
+    end
+          
     private
     
     def call_api(request_uri, request_method, request_data=nil)
+      uri = URI(SECURESERVER + request_uri)
+
+      # which HTTP method to use?
       if request_method == :get
-        response = RestClient.get request_uri, :accept => :text
-      elsif request_method == :post
-        response = RestClient.post request_uri
+        request = Net::HTTP::Get.new uri.request_uri
       elsif request_method == :put
-        response = RestClient.putrequest_uri
+        request = Net::HTTP::Put.new uri.request_uri
+        request.body = make_anvl(request_data)
+      elsif request_method == :post
+        request = Net::HTTP::Post.new uri.request_uri
+        request.body = make_anvl(request_data)
       elsif request_method == :delete
-        response = RestClient.delete request_uri
+        request = Net::HTTP::Delete.new uri.request_uri
       end
-      parse_record response
+      
+      request.basic_auth @user, @pass
+      request.add_field("Content-Type", "text/plain; charset=UTF-8")
+
+      # Make the call
+      Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+        parse_record http.request(request).body
+      end
     end
 
     def parse_record(ezid_response)
